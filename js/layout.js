56d8e5a872fc3937a153d82d26d604f3438d8076
Valentina Andrada Perino <vandradaperino@frba.utn.edu.ar>
2026-08-14 11:32:42 -0300

    fix: paths de navegación - Corrige las rutas del header y footer desde layout.js para evitar paths hardcodeados y permitir la navegación desde distintas vistas

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,18 @@
+// Variables para paths en Header y Footer
+const scriptUrl = new URL(document.currentScript.src);
+const projectRoot = new URL("../", scriptUrl);
+
 async function loadHeader() {
-    const resp = await fetch("/vistas/layout/header.html");
+    const resp = await fetch(new URL("/vistas/layout/header.html", projectRoot));
     document.getElementById("header-placeholder").outerHTML = await resp.text();
+
+    configureHeaderLinks();
+
     setActiveNavLink();
 }
 
 async function loadFooter() {
-    const resp = await fetch("/vistas/layout/footer.html");
+    const resp = await fetch(new URL("/vistas/layout/footer.html", projectRoot));
     document.getElementById("footer-placeholder").outerHTML = await resp.text();
     setFooterYear();
 }
@@ -25,6 +32,23 @@ function setFooterYear() {
     if (yearEl) {
         yearEl.textContent = new Date().getFullYear();
     }
+}
+
+// Función para configurar paths/rutas en Header
+function configureHeaderLinks() {
+    const homeLink = document.querySelector('[data-route="home"]');
+    const productosLink = document.querySelector('[data-route="productos"]');
+    const contactoLink = document.querySelector('[data-route="contacto"]');
+    const logoLink = document.querySelector(".navbar-brand");
+
+    const logo = document.querySelector(".logo");
+    logo.src = new URL("assets/logo/logo.svg", projectRoot);
+
+    logoLink.href = new URL("index.html", projectRoot);
+
+    homeLink.href = new URL("index.html", projectRoot);
+    productosLink.href = new URL("vistas/productos.html", projectRoot);
+    contactoLink.href = new URL("vistas/contacto.html", projectRoot);
 }
 
 loadHeader();
